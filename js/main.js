5601ad2cbcfe5cb0cbf2bd6e7e7ff8234501b5e5
smboolean76 <smboolean76@madrigali.dev>
2022-11-07 10:59:45 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// function hello() {
+//     console.log('ciao dal setTimeout');
+// }
+// function hello2() {
+//     console.log('ciao dal secondo setTimeout');
+// }
+
+// setTimeout(hello2, 1);
+// setTimeout(hello, 1);
+
+// console.log('ciao a tutti');
+
+// for(let i = 0; i < 10000; i++) {
+//     console.log(i);
+// }
+
+
+
+const numerbs = [1,2,3];
+
+document.getElementById('pino').innerHTML = numerbs;
